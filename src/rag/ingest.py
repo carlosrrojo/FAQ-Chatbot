953c9530d087b292b39dabdd58ac2b0f6e468ced
Carlos Rojo <carlos.rojo@udc.es>
2026-02-23 18:26:44 +0100

    Added different ingest strategies

--- src/rag/ingest.py
+++ src/rag/ingest.py
@@ -1,5 +1,4 @@
 import os
-import time
 from langchain_community.document_loaders import TextLoader, DirectoryLoader, PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter, MarkdownHeaderTextSplitter
 from langchain_experimental.text_splitter import SemanticChunker
@@ -8,7 +7,7 @@ from langchain_ollama import OllamaEmbeddings, ChatOllama
 from langchain_core.documents import Document
 from langchain_core.prompts import ChatPromptTemplate
 import chromadb
-
+import sys
 
 # Configuration
 DATA_PATH = "data/documents"
@@ -30,43 +29,109 @@ def reset_db():
 def extract_metadata(content: str, llm: ChatOllama) -> dict:
     pass
 
-def ingest_docs(clear_db=False):
+def load_documents(format: str = "txt") -> list[Document]:
+    print(f"Loading documents from {DATA_PATH}...")
+    # Loading docs
+    docs = []
+    loader = None
+    for file in os.listdir(DATA_PATH):
+        if format=="txt" and file.endswith("txt"):
+            loader = TextLoader(os.path.join(DATA_PATH, file))
+        elif format=="pdf" and file.endswith("pdf"):
+            loader = PyPDFLoader(os.path.join(DATA_PATH, file))
+        elif format=="md" and file.endswith("md"):
+            loader = TextLoader(os.path.join(DATA_PATH, file)) # Using TextLoader instead of undefined MarkdownLoader
+        if loader == None:
+            continue
+        docs.extend(loader.load())
+    return docs
+
+# Process image, tables and other types of data (not neccessary for now)
+def process_documents(docs: list[Document]) -> list[Document]:
+    return docs
+
+#======================
+#= CHUNKING STRATEGIES=
+#======================
+def md_chunking_strategie(docs):
+    headers_to_split_on = [
+        ("#", "Header 1"),
+        ("##", "Header 2"),
+        ("###", "Header 3"),
+        ("####", "Header 4"),
+    ]
+    md_splitter = MarkdownHeaderTextSplitter(headers_to_split_on=headers_to_split_on)
+    
+    md_docs = []
+    for doc in docs:
+        # MarkdownHeaderTextSplitter works on raw text and returns Document objects with header metadata
+        splits = md_splitter.split_text(doc.page_content)
+        for split in splits:
+            # Merge original document metadata (like 'source') with the new header metadata
+            split.metadata.update(doc.metadata)
+            md_docs.append(split)
+    return md_docs
+
+def naive_chunking_strategie(docs):
+    text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1024,  # chunk size (characters)
+            chunk_overlap=256,  # chunk overlap (characters)
+            add_start_index=True,  # track index in original document
+        )
+    all_splits = text_splitter.split_documents(docs)
+    return all_splits
+
+def semantic_chunking_strategie(docs, embeddings):
+    text_splitter = SemanticChunker(embeddings, breakpoint_threshold_type="percentile")
+    all_splits = text_splitter.split_documents(docs)
+    return all_splits
+
+def ingest_docs(clear_db=False, strategy="recursive"):
     if clear_db:
         reset_db()
     embeddings = OllamaEmbeddings(model=MODEL_NAME)
     vector_store = Chroma(
-        collection_name="espazo_nature",
+        collection_name=strategy+"_"+COLLECTION_NAME,
         embedding_function=embeddings,
         persist_directory=DB_PATH
     )
-    # Loading docs
-    #loader = PyPDFLoader(os.path.join(DATA_PATH, "INFORMACIÓN PARA EL BOT.pdf")) # Hardcoded for now
-    loader = TextLoader(os.path.join(DATA_PATH, "espazo_nature.txt"))
-    docs = loader.load()
-
+    if strategy == "md":
+        docs = load_documents("md")
+    else:
+        docs = load_documents("pdf")
     assert len(docs) > 0, "No documents loaded"
-    
-    print(f"Total characters: {len(docs[0].page_content)}")
-    print(docs[0].page_content[:500])
-    
-    # Splitting docs
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1024,  # chunk size (characters)
-        chunk_overlap=102,  # chunk overlap (characters)
-        add_start_index=True,  # track index in original document
-    )
-    all_splits = text_splitter.split_documents(docs)
+    print(f"Loaded {len(docs)} documents.")
 
-    print(f"Split blog post into {len(all_splits)} sub-documents.")
+    characters = 0
+    for doc in docs:
+        characters += len(doc.page_content)
+        print(doc.metadata)
+    
+    print(f"Total characters: {characters}")
+
+    docs_processed = process_documents(docs)
+
+    # Chunking process
+    if strategy == "md":
+        all_splits = md_chunking_strategie(docs_processed)
+    elif strategy == "recursive":
+        all_splits = naive_chunking_strategie(docs_processed)
+    elif strategy == "semantic":
+        all_splits = semantic_chunking_strategie(docs_processed, embeddings)
+    else:
+        raise ValueError(f"Unknown chunking strategy: {strategy}")
+
+
+    print(f"Split document into {len(all_splits)} sub-documents using {strategy}.")
 
     # Storing documents
     document_ids = vector_store.add_documents(documents=all_splits)
     print(document_ids[:3])
 
-
 if __name__ == "__main__":
     from langchain_core.globals import set_debug
     from dotenv import load_dotenv
-    set_debug(True)
+    set_debug(False)
     load_dotenv()
-    ingest_docs(clear_db=True)
+    for i in ["recursive","md","semantic"]:
+        ingest_docs(clear_db=False, strategy=i)
