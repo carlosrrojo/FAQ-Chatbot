7c462921f99557e4fd1e02f7f1775322b72d2797
carlosrrojo <carlosrojor98@gmail.com>
2026-04-02 16:41:58 +0200

    Añadido referencias en la ingestion

--- src/rag/ingest.py
+++ src/rag/ingest.py
@@ -1,12 +1,13 @@
-from src.rag.metadata_extractor import MetadataExtractor, enrich_document
-import glob as glob_module
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+import os
+from langchain_community.document_loaders import TextLoader, DirectoryLoader, PyPDFLoader, PyMuPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter, MarkdownHeaderTextSplitter
 from langchain_chroma import Chroma
 from langchain_ollama import OllamaEmbeddings
 from langchain_core.documents import Document
-from src.rag.processor import parse_doc, get_siblings
-from src.rag.config import MODEL_NAME, DATA_PATH, DB_PATH, COLLECTION
-# from processor import add_metadata_keyBERT
+from langchain_core.prompts import ChatPromptTemplate
+import chromadb
+from src.rag.extract_processor import ExtractProcessor
+import json
 
 CHUNK_SIZE      = 1024
 CHUNK_OVERLAP   = 256
@@ -47,6 +48,84 @@ vectorstore = Chroma(
     persist_directory=DB_PATH,
 )
 
-print(f"ingested to: {COLLECTION} in {DB_PATH}")
-document_ids = vectorstore.add_documents(chunks)
-print(f"Stored {len(document_ids)} documents using {CHUNK_SIZE}/{CHUNK_OVERLAP} split.")
+def recursive_chunking_strategie(docs):
+    text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1024,  # chunk size (characters)
+            chunk_overlap=256,  # chunk overlap (characters)
+            add_start_index=True,  # track index in original document
+        )
+    all_splits = text_splitter.split_documents(docs)
+    return all_splits
+
+def ingest_docs(clear_db=False, strategy="recursive"):
+    if clear_db:
+        reset_db(strategy)
+
+    embeddings = OllamaEmbeddings(model=MODEL_NAME)
+    vector_store = Chroma(
+        collection_name=strategy+"_"+COLLECTION_NAME,
+        embedding_function=embeddings,
+        persist_directory=DB_PATH
+    )
+
+    llm = ChatOllama(model=MODEL_NAME)
+    extract_processor = ExtractProcessor(llm)
+
+    if strategy == "md":
+        docs = load_documents("md")
+    else:
+        docs = load_documents("pdf")
+    assert len(docs) > 0, "No documents loaded"
+    print(f"Loaded {len(docs)} documents.")
+
+    # Divide documents into sections by headings
+    print(f"Dividing documents into sections...")
+    docs_processed = []
+    for doc in docs:
+        docs_processed.extend(extract_processor.process_document(doc))
+    print(f"Processed documents into {len(docs_processed)} sections.")
+    for doc in docs_processed:
+        print(doc.metadata['section'])
+
+    # Chunking process
+    if strategy == "md":
+        all_splits = md_chunking_strategie(docs_processed)
+    elif strategy == "recursive":
+        all_splits = recursive_chunking_strategie(docs_processed)
+    else:
+        raise ValueError(f"Unknown chunking strategy: {strategy}")
+
+    print(f"Split document into {len(all_splits)} sub-documents using {strategy}.")
+
+    # Metadata extraction with LLM
+    print("Extracting keywords for metadata...")
+    docs_with_metadata = []
+    for doc in all_splits:
+        keywords = extract_processor.extract_metadata(doc.page_content)
+        siblings = extract_processor.get_siblings(all_splits, doc.metadata["parent_section"])
+        doc.page_content = f"""
+        [KEYWORDS]
+        {" , ".join(keywords)}
+        [CONTENT]
+        {doc.page_content}
+        [REFERENCES]
+        {siblings}
+        """
+        doc.metadata.update({"keywords": json.dumps(keywords)})
+        docs_with_metadata.append(doc)
+    
+    print("Added metadata to documents.")
+
+    # Storing documents
+    print("Storing documents in vector store...")
+    document_ids = vector_store.add_documents(documents=docs_with_metadata)
+    print(f"Stored {len(document_ids)} documents.")
+
+if __name__ == "__main__":
+    from langchain_core.globals import set_debug
+    from dotenv import load_dotenv
+    set_debug(False)
+    load_dotenv()
+    ingest_docs(clear_db=False, strategy="recursive")
+    #for i in ["md","semantic"]:
+    #    ingest_docs(clear_db=True, strategy=i)
